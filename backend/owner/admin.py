db4819fb3787a00f1a83d1620164ce912728f640
Vercixxx <krz.sluzalek@gmail.com>
2023-08-30 17:43:19 +0200

    Added models to admin panel

--- backend/owner/admin.py
+++ backend/owner/admin.py
@@ -1,2 +1,6 @@
 from django.contrib import admin
+from .models import Owner
 
+@admin.register(Owner)
+class YourModelAdmin(admin.ModelAdmin):
+    list_display = ['username', 'user_role', 'is_active']
