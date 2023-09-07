47e2e0016dbac92dcaa17a0ca0ee711c05a934ff
Vercixxx <krz.sluzalek@gmail.com>
2023-09-07 23:09:39 +0200

    Added log-out prompt. Moved buttons in user list site. Redesign table of users.

--- frontend/src/components/hr/ModifyUser.vue
+++ frontend/src/components/hr/ModifyUser.vue
@@ -1,8 +1,50 @@
 <template>
   <div>
 
+    <p class="p-2 fw-bolder fs-5">Filters</p>
     <!-- Search bar section -->
-    <div class="text-center mb-5 d-flex justify-content-end">
+    <div class="text-center mb-5 d-flex justify-content-between">
+
+      <div class="p-2">
+        <div class="btn-group dropdown mx-2">
+          <span class="input-group-text rounded-0 rounded-start" id="basic-addon1">User role</span>
+
+          <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            {{ selectedRole }}
+          </button>
+          <ul class="dropdown-menu">
+            <li @click="selectedRole = 'All'; loadUsers();"><a class="dropdown-item fw-bolder ">All</a></li>
+            <li @click="selectedRole = 'HR'; loadUsers();"><a class="dropdown-item ">HR</a></li>
+            <li @click="selectedRole = 'Asset'; loadUsers();"><a class="dropdown-item ">Asset</a></li>
+            <li @click="selectedRole = 'Payroll'; loadUsers();"><a class="dropdown-item ">Payroll</a></li>
+            <li @click="selectedRole = 'Client'; loadUsers();"><a class="dropdown-item ">Client</a></li>
+            <li @click="selectedRole = 'Manager'; loadUsers();"><a class="dropdown-item ">Manager</a></li>
+            <li @click="selectedRole = 'Driver'; loadUsers();"><a class="dropdown-item ">Driver</a></li>
+          </ul>
+        </div>
+
+
+
+        <div class="btn-group dropdown">
+          <span class="input-group-text rounded-0 rounded-start" id="basic-addon1">User status</span>
+
+          <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-bs-toggle="dropdown"
+            aria-expanded="false">
+
+            <span v-if="selectedActive === 'True'">Yes</span>
+            <span v-else-if="selectedActive === 'False'">No</span>
+            <span v-else>All</span>
+          </button>
+          <ul class="dropdown-menu">
+            <li @click="selectedActive = 'All'; loadUsers();"><a class="dropdown-item fw-bolder ">All</a></li>
+            <li @click="selectedActive = 'True'; loadUsers();"><a class="dropdown-item ">Active</a></li>
+            <li @click="selectedActive = 'False'; loadUsers();"><a class="dropdown-item ">Not active</a></li>
+          </ul>
+        </div>
+
+      </div>
+
 
       <form role="search" method="POST" action="" @submit.prevent="search">
 
@@ -15,7 +57,6 @@
               <path
                 d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
             </svg>
-            Search
           </button>
           <button type="button" class="btn btn-outline-success dropdown-toggle dropdown-toggle-split"
             data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
@@ -49,55 +90,44 @@
 
     <!-- End of search bar section -->
 
-
-
-
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Username</th>
-          <th>Email</th>
-          <th>
-            User role
-            <div class="btn-group dropend">
-              <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                {{ selectedRole }}
-              </button>
-              <ul class="dropdown-menu">
-                <li @click="selectedRole = 'all' ; loadUsers();"><a class="dropdown-item fw-bolder ">All</a></li>
-                <li @click="selectedRole = 'HR' ; loadUsers();"><a class="dropdown-item ">HR</a></li>
-                <li @click="selectedRole = 'Asset' ; loadUsers();"><a class="dropdown-item ">Asset</a></li>
-                <li @click="selectedRole = 'Payroll' ; loadUsers();"><a class="dropdown-item ">Payroll</a></li>
-                <li @click="selectedRole = 'Client' ; loadUsers();"><a class="dropdown-item ">Client</a></li>
-                <li @click="selectedRole = 'Manager' ; loadUsers();"><a class="dropdown-item ">Manager</a></li>
-                <li @click="selectedRole = 'Driver' ; loadUsers();"><a class="dropdown-item ">Driver</a></li>
-              </ul>
-            </div>
-          </th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="user in users" :key="user.id">
-          <td>{{ user.username }}</td>
-          <td>{{ user.email }}</td>
-          <td>{{ user.user_role }}</td>
-          <td>
-            <button class="btn btn-outline-secondary me-3" @click="editUser(user)">Edit</button>
-            <button class="btn btn-outline-danger"  @click="deleteConfirm(user.username, user.user_role)">Delete</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <div v-if="users.length === 0">
-      No users in database.
+    <div v-if="users.length === 0" class="text-danger fs-2">
+      No records.
     </div>
+
+    <div v-else class="table-responsive">
+      <table class="table table-hover table-striped">
+        <thead>
+          <tr class="text-center">
+            <th>USERNAME</th>
+            <th>EMAIL</th>
+            <th>USER ROLE</th>
+            <th>ACTIVE</th>
+            <th>ACTION</th>
+          </tr>
+        </thead>
+        <tbody class="table-group-divider">
+          <tr v-for="user in users" :key="user.id" class="text-center">
+            <td>{{ user.username }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.user_role }}</td>
+            <td>
+              <span v-if="user.is_active">Yes</span>
+              <span v-else>No</span>
+            </td>
+            <td>
+              <button class="btn btn-outline-secondary m-3" @click="editUser(user)">Edit</button>
+              <button class="btn btn-outline-danger" @click="deleteConfirm(user.username, user.user_role)">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
 
     <!-- Confirmation modal -->
     <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#confirmModal" id="confirm_modal_button"
-      style="display: none;">
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#confirmModal"
+      id="confirm_modal_button" style="display: none;">
     </button>
 
     <!-- Modal -->
@@ -109,13 +139,14 @@
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body text-danger fs-5">
-            <p> 
+            <p>
               You are trying to delete {{ dataConfirmUsername }}, are you sure?
             </p>
           </div>
           <div class="modal-footer d-flex justify-content-center">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
-            <button type="button" class="btn btn-danger" data-bs-dismiss="modal"  @click="deleteUser(dataConfirmUsername, dataConfirmUserRole)">Yes</button>
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal"
+              @click="deleteUser(dataConfirmUsername, dataConfirmUserRole)">Yes</button>
           </div>
         </div>
       </div>
@@ -134,7 +165,8 @@
               <p v-if="dataError">Error</p>
               <p v-else>Success</p>
             </h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"  @click='loadUsers'></button>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+              @click='loadUsers'></button>
           </div>
           <div class="modal-body text-center text-danger">
             <div v-if="dataError">
@@ -152,10 +184,9 @@
       </div>
     </div>
     <!-- Message modal -->
-    <form>
 
-      <button type="button" class="btn btn-primary"  @click="reloadComponent">reload</button>
-    </form>
+      <button type="button" class="btn btn-primary" @click="reloadComponent">reload</button>
+
 
   </div>
 </template>
@@ -174,7 +205,8 @@ export default {
       dataSuccess: null,
       dataConfirmUsername: null,
       dataConfirmUserRole: null,
-      selectedRole: 'all',
+      selectedRole: 'All',
+      selectedActive: 'All',
       query_data: {},
     };
   },
@@ -187,10 +219,10 @@ export default {
       const message = localStorage.getItem('message');
       console.log(message);
       localStorage.removeItem('message');
-    //   this.dataError = message;
-    //   document.getElementById('hiddenButton').click();
-    // this.dataError = message;
-    //       document.getElementById('hiddenButton').click();
+      //   this.dataError = message;
+      //   document.getElementById('hiddenButton').click();
+      // this.dataError = message;
+      //       document.getElementById('hiddenButton').click();
 
     }
   },
@@ -199,10 +231,11 @@ export default {
     async loadUsers() {
       try {
         const query_data = {
-          role : this.selectedRole,
+          role: this.selectedRole,
+          active: this.selectedActive,
         }
 
-        const response = await axios.get(`get-users/${query_data.role}/`);
+        const response = await axios.get(`get-users/${query_data.role}/${query_data.active}/`);
 
         this.users = response.data;
         console.log(response.data)
@@ -237,12 +270,12 @@ export default {
     },
 
     editUser(user) {
-      
+
     },
 
     search() {
       console.log(this.search_content)
-    },  
+    },
 
     reloadComponent() {
       this.loadUsers();
