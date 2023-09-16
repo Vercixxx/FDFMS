f43c5b195c4e9b9da1154bf8aa6f021d416298b1
Vercixxx <krz.sluzalek@gmail.com>
2023-09-16 23:52:15 +0200

    Added JWT authorization header. Added Secure for entering views ( only for logged users, by auth-guard). Added protecion for classes (avalible only for logged users)

--- backend/car/urls.py
+++ backend/car/urls.py
@@ -4,19 +4,19 @@ from . import views
 # Rest
 from rest_framework.routers import DefaultRouter
 
-car_router = DefaultRouter()
-car_router.register(r"get-cars", views.CarsViewSet, basename='get-cars')
+# car_router = DefaultRouter()
+# car_router.register(r"get-cars", views.CarsViewSet, basename='get-cars')
 
 
 
 
 urlpatterns = [
-    path('api/cars/', include(car_router.urls)),
+    # path('api/cars/', include(car_router.urls)),
     
     # Creating car
-    path('api/car', views.CarView.as_view(), name="cars"),
+    # path('api/car', views.CarView.as_view(), name="cars"),
     
     # Delete car
-    path('api/car/delete/<str:vin>', views.DeleteCar.as_view(), name="delete-cars"),
+    # path('api/car/delete/<str:vin>', views.DeleteCar.as_view(), name="delete-cars"),
     
 ]
