09639766fcbe6f07bad0b34818697c13a03dc689
Vercixxx <krz.sluzalek@gmail.com>
2023-09-21 21:22:33 +0200

    Moved from cli to Vite

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
 
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
     "http://127.0.0.1:8000",
