3f26e39ff01363bb29b4b990f7fa3fe4aa6979d4
Vercixxx <krz.sluzalek@gmail.com>
2024-01-10 10:25:14 +0100

    Wokring on Country/States

--- backend/other/urls.py
+++ backend/other/urls.py
@@ -3,8 +3,22 @@ from . import views
 
 
 urlpatterns = [
+    # Countries
+    
+    # Add Country
+    path('api/countries/add/', views.AddCountry.as_view(), name="add-country"),
+    
+    # Delete Country
+    path('api/countries/delete/<str:name>/', views.DeleteCountry.as_view(), name="delete-country"),
+    
+    # Edit Country
+    path('api/countries/edit/<str:name>/', views.EditCountry.as_view(), name="edit-country"),
+    
     # GetCountries
     path('api/countries/get/', views.GetCountries.as_view(), name="get-countries"),
+    
+    
+    # Countries
     
     # GetStates
     path('api/states/get/', views.GetStates.as_view(), name="get-states"),
--- backend/other/views.py
+++ backend/other/views.py
@@ -9,30 +9,76 @@ from .serializers import *
 
 class GetCountries(APIView):
     permission_classes = [IsAuthenticated]
-    
+
     def get(self, request):
         countries = Country.objects.all().order_by('name')
         serialized = CountrySerializer(countries, many=True)
         return JsonResponse(serialized.data, safe=False, status=200)
-    
-    
+
+
 class GetStates(APIView):
     permission_classes = [IsAuthenticated]
-    
+
     def get(self, request):
         country = self.request.query_params.get('country', '').strip()
-        
+
         output = []
         if country:
             states = State.objects.filter(country__name=country)
             serialized = StateSerializer(states, many=True)
             output = serialized.data
-            
+
         else:
             states = State.objects.all().order_by('name')
             serialized = StateSerializer(states, many=True)
             output = serialized.data
-            
+
         return JsonResponse(output, safe=False, status=200)
+
+
+class AddCountry(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        name = self.request.data['name'].strip()
+
+        if name:
+            if Country.objects.filter(name=name).exists():
+                return JsonResponse({'error': 'Country already exists'}, safe=False, status=400)
+            country = Country.objects.create(name=name)
+            return JsonResponse({'message': 'Ok'}, status=201)
+
+        return JsonResponse({'error': 'Country name is required'}, safe=False, status=400)
+
+class DeleteCountry(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, name):
+        name = name.strip()
+
+        if name:
+            country = Country.objects.get(name=name)
+            country.delete()
+            return JsonResponse({'message': 'Ok'}, status=201)
+        
+        return JsonResponse({'error': 'Country name is required'}, safe=False, status=400)
     
-    
+class EditCountry(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, name):
+        name = name.strip()
+        print(name)
+        print(request.data)
+        if name:
+            country = Country.objects.get(name=name)
+
+            serialized = CountrySerializer(country, data=request.data, partial=True)
+            
+            if serialized.is_valid():
+                serialized.save()
+                return JsonResponse({'message': 'Ok'}, status=200)
+            else:
+                return JsonResponse(serialized.errors, status=400)
+        
+        return JsonResponse({'error': 'Country name is required'}, safe=False, status=400)
